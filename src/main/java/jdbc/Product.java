4d9a6fcb259b2c6e6ffb17016d246538f71eee1f
kishikishore2863 <kishikishore.2863@gmail.com>
2025-06-22 23:04:35 +0530

    jdbc

--- src/main/java/jdbc/Product.java
+++ src/main/java/jdbc/Product.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+import java.sql.PreparedStatement;
 
 public class Product {
     public static void main(String[] args) throws SQLException {
@@ -23,26 +24,37 @@ public class Product {
 //        statement.addBatch(updateQuery);
 //        int[] arr = statement.executeBatch();
 //        System.out.println("number of query affected "+arr.length);
+        insert(connection);
     }
 
-//    public void insert(Connection connection) throws SQLException {
-//        String sql = "insert into product(id,pname,quantity,price,delivery) values(?,?,?,?)";
-//        PreparedStatement statement = connection.prepareStatement(sql);
-//        System.out.println("enter the number of products insertion :");
-//        Scanner scanner = new Scanner(System.in);
-//        int numbers = scanner.nextInt();
-//        for (int i=0; i<numbers; i++){
-//            String sql = "insert into product(id,pname,quantity,price,delivery) values(?,?,?,?)";
-//            int id = scanner.nextInt();
-//            String product = scanner.nextLine();
-//            int quantity =scanner.nextInt();
-//            int price =scanner.nextInt();
-//            String delivery = scanner.nextLine();
-//            statement.setInt()
-//        }
-//
-//
-//    }
+    public static void insert(Connection connection) throws SQLException {
+        String sql = "insert into product(id,pname,quantity,price,delivery) values(?,?,?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        System.out.println("enter the number of products insertion :");
+        Scanner scanner = new Scanner(System.in);
+        int numbers = scanner.nextInt();
+        for (int i=0; i<numbers; i++){
+            System.out.print("Enter product ID: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Enter product name: ");
+            String product = scanner.nextLine();
+            System.out.print("Enter quantity: ");
+            int quantity = scanner.nextInt();
+            System.out.print("Enter price: ");
+            int price = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Enter delivery location: ");
+            String delivery = scanner.nextLine();
+            statement.setInt(1, id);
+            statement.setString(2, product);
+            statement.setInt(3, quantity);
+            statement.setInt(4, price);
+            statement.setString(5, delivery);
+            statement.executeUpdate();
+            System.out.println("Inserted product with id: " + id);
+        }
+    }
 
 
 }
